d49fc4d0f23cbb89a84bc432bed10ee151976e4e
Mikkel H <mikkel_fhammer@hotmail.com>
2020-11-18 22:49:13 +0100

    Added admin pages for managing experiences, removed unnecessary double client in homecontroller

---
+++ OplevOgDel.Web/Models/DTO/ManageExperienceDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OplevOgDel.Web.Models.DTO
+{
+    public class ManageExperienceDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public CategoryDTO Category { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public List<ExperienceReportDTO> ExperienceReports { get; set; }
+        public List<NewReviewDTO> Reviews { get; set; }
+    }
+}
