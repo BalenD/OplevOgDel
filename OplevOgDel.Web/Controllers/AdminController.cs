c78328827f0d9a46dcae08ea91e6f67576c1bc39
Mikkel H <mikkel_fhammer@hotmail.com>
2020-11-22 19:41:15 +0100

    working on editexperience

--- OplevOgDel.Web/Controllers/AdminController.cs
+++ OplevOgDel.Web/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using OplevOgDel.Web.Models.DTO;
 using OplevOgDel.Web.Models.ViewModel;
@@ -43,11 +44,63 @@ namespace OplevOgDel.Web.Controllers
                 if (response.IsSuccessStatusCode)
                 {
                     var result = await response.Content.ReadAsStringAsync();
-                    viewModel.Experience = JsonConvert.DeserializeObject<ManageExperienceDTO>(result);
+                    viewModel = JsonConvert.DeserializeObject<ManageExperienceViewModel>(result);
                 }
             }
 
             return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditExperienceAsync(Guid id)
+        {
+            string experienceEndPoint = "https://localhost:44360/" + "api/experiences/" + id;
+            string categoriesEndPoint = "https://localhost:44360/" + "api/categories";
+
+            EditExperienceViewModel viewModel = new EditExperienceViewModel();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage experienceResponse = await client.GetAsync(experienceEndPoint);
+                if (experienceResponse.IsSuccessStatusCode)
+                {
+                    var result = await experienceResponse.Content.ReadAsStringAsync();
+                    viewModel.Experience = JsonConvert.DeserializeObject<ExperienceDTO>(result);
+                }
+
+                HttpResponseMessage categoriesResponse = await client.GetAsync(categoriesEndPoint);
+                if (categoriesResponse.IsSuccessStatusCode)
+                {
+                    var result = await categoriesResponse.Content.ReadAsStringAsync();
+                    var categoriesTest = JsonConvert.DeserializeObject<List<CategoryDTO>>(result);
+                    viewModel.Categories = categoriesTest.Select(c => new SelectListItem() { Value = c.Id.ToString(), Text = c.Name });
+                }
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditExperienceAsync(Guid id, ExperienceDTO experience)
+        {
+            string experienceEndPoint = "https://localhost:44360/" + "api/experiences/" + id;
+
+            var content = new StringContent(JsonConvert.SerializeObject(experience), System.Text.Encoding.UTF8, "application/json");
+
+            //ExperienceDTO viewModel = new ExperienceDTO();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.PutAsync(experienceEndPoint, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    //var result = await response.Content.ReadAsStringAsync();
+                    //viewModel = JsonConvert.DeserializeObject<ExperienceDTO>(result);
+                    return View();
+                }
+            }
+
+            return View();
         }
     }
 }
---
+++ OplevOgDel.Web/Models/ViewModel/EditExperienceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using OplevOgDel.Web.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OplevOgDel.Web.Models.ViewModel
+{
+    public class EditExperienceViewModel
+    {
+        public ExperienceDTO Experience { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+    }
+}
